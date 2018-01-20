e4a003c1c243b26e902b39fbcf6729479166f2ae
AaronLopez <aaronlo2018@gmail.com>
2018-01-20 15:18:18 -0600

    test: add code coverage

---
+++ src/index.js
@@ -0,0 +1,21 @@
+/**
+ *  jquery-bg-pexels
+ *  @version 0.0.0-development
+ *  @author Aaron Lopez
+ * @license The MIT License (MIT)
+*/
+
+(function($){
+
+  $.fn.bgUnsplash = function(){
+    $(this).css({
+      width:'100%',
+      height: '100vh',
+      minHeight: '800px',
+    });
+    return $(this);
+  };
+
+})(jQuery);
+
+
