d89234ab59ea0ed726778995ba136ddac9bf9ee3
AaronLopez <aaronlo2018@gmail.com>
2018-01-27 14:28:46 -0600

    feat: add random photo

--- src/index.js
+++ src/index.js
@@ -5,17 +5,54 @@
  * @license The MIT License (MIT)
 */
 
-(function($){
+(function(global,$){
+  function BgPexels(){
 
-  $.fn.pexels = function(){
-    $(this).css({
-      width:'100%',
-      height: '100vh',
-      minHeight: '800px',
-    });
-    return $(this);
+  }
+
+  BgPexels.prototype.setup = function(clientId){
+    this.clientId = clientId;
   };
 
-})(jQuery);
+  var getRandomPhoto = function(){
+    return (
+      $.ajax({
+        url: 'https://api.unsplash.com/photos/random',
+        beforeSend: function(xhr){
+          xhr.setRequestHeader('Authorization','Client-ID '+  global.BgPexels.clientId);
+        }
+      })
+    );
+  };
 
+  $.fn.BgPexels = function(options){
+    options = options || {};
+    var def = $.Deferred();
+    var $self = $(this);
+    $self.css({
+      width:'100%',
+      height: options.height || '100vh',
+      minHeight: options.minHeight || '800px',
+      backgroundColor: options.backgroundColor || 'black',
+      backgroundPosition: options.backgroundPosition || 'center',
+      backgroundSize: options.backgroundSize || 'cover',
+    });
+    getRandomPhoto()
+      .then(function(photo){
+        $self.css({
+          backgroundImage: 'url(' + photo.urls.regular +  ')'
+        });
+        def.resolve($self);
+      })
+      .catch(function(){
+        $self.css({
+          backgroundImage: 'url(' + options.backgroundImage +  ')'
+        });
+        def.reject($self);
+      });
+    return def.promise();
+  };
 
+  global.BgPexels = new BgPexels();
+
+})(window, jQuery);
--- test/index.test.js
+++ test/index.test.js
@@ -4,25 +4,62 @@ var jsdom = require('jsdom');
 var dom = new jsdom.JSDOM('<html><body><section></section></body></html>');
 var $ = global.jQuery =require('jquery')(dom.window);
 
+require('jsdom-global')();
 require('../src');
 
 var $section;
 
 describe('jquery-bg-pexels', function(){
   beforeEach(function(){
+    window.BgPexels.setup('d2034284409ef335282ee5191ba0298788f1f7ae2db7b729ef5afcd9ef3f3bb7');
     $section = $('section');
-    $section.pexels();
   });
 
-  it('should have a width of 100%',function(){
+  it('should have the default values',function(){
+    $section.BgPexels();
     expect($section.css('width')).to.be('100%');
-  });
-
-  it('should have a min-height of 800px',function(){
     expect($section.css('minHeight')).to.be('800px');
+    expect($section.css('backgroundColor')).to.be('black');
+    expect($section.css('backgroundPosition')).to.be('center');
+    expect($section.css('backgroundSize')).to.be('cover');
   });
 
+  it('should have the defined values',function(){
+    $section.BgPexels({
+      minHeight: '700px',
+      backgroundColor: 'red',
+      backgroundPosition:'center',
+      backgroundSize: 'contain'
+    });
+    expect($section.css('width')).to.be('100%');
+    expect($section.css('minHeight')).to.be('700px');
+    expect($section.css('backgroundColor')).to.be('red');
+    expect($section.css('backgroundPosition')).to.be('center');
+    expect($section.css('backgroundSize')).to.be('contain');
+  });
 
+  /* it('should set client id attr', function(){
+    expect(window.BgPexels.clientId).to.be('123');
+  }); */
 
+  it('Should set a random image from Unsplash',function(){
+    return $section.BgPexels({
+      backgroundImage: 'path/defaultImage.jpg',
+      usePromise: true
+    }).then(function($this){
+      expect($this.css('backgroundImage')).to.contain('url');
+    }).catch(function($this){
+      expect($this.css('backgroundImage')).to.contain('path/defaultImage.jpg');
+    });
+  });
+
+  it('Should set default image',function(){
+    window.BgPexels.setup(123);
+    return $section.BgPexels({
+      backgroundImage: 'path/defaultImage.jpg',
+    }).catch(function($this){
+      expect($this.css('backgroundImage')).to.contain('path/defaultImage.jpg');
+    });
+  });
 
 });
